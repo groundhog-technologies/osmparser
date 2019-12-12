a46d1454374a509f891e04874eb0793cf8019d08
jneo8 <jneo8lin@gmail.com>
2019-12-13 00:31:10 +0800

    Finish classifier

--- pkg/classifier/main.go
+++ pkg/classifier/main.go
@@ -36,6 +36,54 @@ func (c *H3Cell) CellToClustersCoordinates(finalCodes []string) clusters.Coordin
 
 // Run .
 func (c *AreaClassifier) Run() error {
+	h3Idxs := h3.Polyfill(c.GeoPolygon, c.Resolution)
+
+	// Filter element not in polygon.
+	newElements := []entity.Element{}
+	h3IdxsMap := make(map[h3.H3Index]int)
+	for _, idx := range h3Idxs {
+		h3IdxsMap[idx] = 1
+	}
+	for _, element := range c.Elements {
+		geoFence := []h3.GeoCoord{}
+		elementH3Idxs := []h3.H3Index{}
+		for _, latLng := range element.Points {
+			h3Idx := h3.FromGeo(
+				h3.GeoCoord{
+					Latitude:  latLng.Lat,
+					Longitude: latLng.Lng,
+				},
+				c.Resolution,
+			)
+			elementH3Idxs = append(h3Idxs, h3Idx)
+			geoFence = append(
+				geoFence,
+				h3.GeoCoord{
+					Latitude:  latLng.Lat,
+					Longitude: latLng.Lng,
+				},
+			)
+		}
+		inElementH3Idxs := h3.Polyfill(h3.GeoPolygon{Geofence: geoFence}, c.Resolution)
+		for _, h3Idx := range inElementH3Idxs {
+			elementH3Idxs = append(elementH3Idxs, h3Idx)
+		}
+
+		isInPolygon := true
+		for _, idx := range elementH3Idxs {
+			if _, ok := h3IdxsMap[idx]; !ok {
+				isInPolygon = false
+				break
+			}
+		}
+		if isInPolygon {
+			newElements = append(newElements, element)
+			logrus.Debugf("%#v in polygon", element)
+		}
+	}
+	c.Elements = newElements
+	h3IdxsMap = nil
+
 	// Gen poiTypeMap
 	poiTypeMap := make(map[string]int)
 	for _, element := range c.Elements {
@@ -48,8 +96,8 @@ func (c *AreaClassifier) Run() error {
 		poiFinalCodes = append(poiFinalCodes, k)
 	}
 	sort.Strings(poiFinalCodes)
+	logrus.Info("Finish gen poi finalCode arr")
 
-	h3Idxs := h3.Polyfill(c.GeoPolygon, c.Resolution)
 	cellStatistics := make(map[h3.H3Index]H3Cell)
 	for _, h3Idx := range h3Idxs {
 		cellPOIs := make(map[string]int)
@@ -61,10 +109,12 @@ func (c *AreaClassifier) Run() error {
 			POIs:    cellPOIs,
 		}
 	}
+	logrus.Info("Finish gen cellStatistics")
 
 	for _, element := range c.Elements {
+		logrus.Debug(element)
 		geoFence := []h3.GeoCoord{}
-		h3Idxs := []h3.H3Index{}
+		elementH3Idxs := []h3.H3Index{}
 		for _, latLng := range element.Points {
 			h3Idx := h3.FromGeo(
 				h3.GeoCoord{
@@ -73,7 +123,7 @@ func (c *AreaClassifier) Run() error {
 				},
 				c.Resolution,
 			)
-			h3Idxs = append(h3Idxs, h3Idx)
+			elementH3Idxs = append(h3Idxs, h3Idx)
 			geoFence = append(
 				geoFence,
 				h3.GeoCoord{
@@ -84,10 +134,10 @@ func (c *AreaClassifier) Run() error {
 		}
 		inElementH3Idxs := h3.Polyfill(h3.GeoPolygon{Geofence: geoFence}, c.Resolution)
 		for _, h3Idx := range inElementH3Idxs {
-			h3Idxs = append(h3Idxs, h3Idx)
+			elementH3Idxs = append(elementH3Idxs, h3Idx)
 		}
 
-		for _, h3Idx := range h3Idxs {
+		for _, h3Idx := range elementH3Idxs {
 			if _, ok := cellStatistics[h3Idx]; ok {
 				for _, code := range element.FinalCodes {
 					cellStatistics[h3Idx].POIs[code]++
@@ -96,6 +146,7 @@ func (c *AreaClassifier) Run() error {
 		}
 	}
 
+	logrus.Info("Start kmeans")
 	d := clusters.Observations{}
 	for _, cell := range cellStatistics {
 		d = append(d, cell.CellToClustersCoordinates(poiFinalCodes))

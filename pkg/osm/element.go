c86415c3a56729726a34500a967eb75de2f5f9e5
jneo8 <jneo8lin@gmail.com>
2020-01-31 12:59:44 +0800

    way element to JSON

--- pkg/osm/element.go
+++ pkg/osm/element.go
@@ -59,7 +59,7 @@ func (e *Element) nodeToJSON() []byte {
 	nodeID := "node" + "/" + strconv.FormatInt(e.Node.ID, 10)
 	f.ID = nodeID
 	f.SetProperty("osmid", nodeID)
-	f.SetProperty("osmType", "Node")
+	f.SetProperty("osmType", "node")
 
 	// Add tag to property.
 	for k, v := range e.Node.Tags {
@@ -74,5 +74,50 @@ func (e *Element) nodeToJSON() []byte {
 
 // wayToJSON .
 func (e *Element) wayToJSON() []byte {
-	return []byte{}
+
+	// collect latlon
+	latLngs := [][]float64{}
+	for _, member := range e.Elements {
+		latLngs = append(
+			latLngs,
+			[]float64{member.Node.Lon, member.Node.Lat},
+		)
+	}
+
+	fc := geojson.NewFeatureCollection()
+	var f *geojson.Feature
+
+	// Define is way is polygon or not.
+	var isPolygon bool
+	if val, ok := e.Way.Tags["area"]; ok && val == "yes" {
+		// Is closedPolylines.
+		// https://wiki.openstreetmap.org/wiki/Way#Closed_way
+		if _, ok := e.Way.Tags["highway"]; ok {
+		} else if _, ok := e.Way.Tags["barrier"]; ok {
+		} else {
+			isPolygon = true
+		}
+	}
+
+	if isPolygon {
+		f = geojson.NewPolygonFeature([][][]float64{latLngs})
+	} else {
+		f = geojson.NewLineStringFeature(latLngs)
+	}
+
+	wayID := "way" + "/" + strconv.FormatInt(e.Way.ID, 10)
+	f.ID = wayID
+	f.SetProperty("osmid", wayID)
+	f.SetProperty("osmType", "way")
+
+	// Add tag to property.
+	for k, v := range e.Way.Tags {
+		f.SetProperty(
+			k, v,
+		)
+	}
+	fc.AddFeature(f)
+
+	rawJSON, _ := fc.MarshalJSON()
+	return rawJSON
 }
